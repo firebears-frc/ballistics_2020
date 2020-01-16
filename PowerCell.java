8507be7abea1249bf33578aa77bd6738c271e3ce
Doug Lau <doug.p.lau@gmail.com>
2020-01-15 19:03:51 -0600

    Improved drag calculations.

--- PowerCell.java
+++ PowerCell.java
@@ -10,15 +10,59 @@ public class PowerCell {
 	/** Mass density of air at 20 degC, 1016 hPa, dew point 10 degC */
 	static private final double AIR_MASS_DENSITY = 1.2019;
 
+	/** Viscosity coefficient of air at 20 degC (Pa * s) */
+	static private final double AIR_VISCOSITY_COEFFICIENT = 1.83e-5;
+
+	/** Reference length of power cell (diameter) */
+	static private final double REFERENCE_LENGTH = RADIUS_M * 2;
+
 	/** Cross-sectional area of power cell (square meters) */
 	static private final double REFERENCE_AREA =
 		Math.PI * (RADIUS_M * RADIUS_M);
 
-	/** Drag coefficient FIXME */
-	static private final double DRAG_COEFFICIENT = 0.5;
-
 	/** Physical mass (about 5 ounces) */
-	static public final double MASS_KG = 0.141748;
+	static public final double MASS_KG = 0.14174762;
+
+	/** Table for estimating the drag coefficient from Reynolds number,
+	 * based on a NASA page about drag on a sphere.  */
+	static private final double[][] DRAG_TABLE = {
+		{ 30, 1.5 },
+		{ 100, 1.1 },
+		{ 150, 1.0 },
+		{ 500, 0.7 },
+		{ 1000, 0.6 },
+		{ 2000, 0.55 },
+		{ 25000, 0.55 },
+		{ 50000, 0.52 },
+		{ 60000, 0.5 },
+		{ 80000, 0.4 },
+		{ 90000, 0.3 },
+		{ 100000, 0.25 },
+		{ 150000, 0.2 },
+		{ 200000, 0.21 },
+		{ 1000000, 0.35 },
+		{ 2000000, 0.4 },
+		{ 10000000, 0.46 },
+	};
+
+	/** Estimate the drag coefficient from reynolds number */
+	static private double dragCoefficient(double rn) {
+		double r0 = 0.0;
+		double cd0 = 2.0;
+		for (int i = 0; i < DRAG_TABLE.length; i++) {
+			double r1 = DRAG_TABLE[i][0];
+			double cd1 = DRAG_TABLE[i][1];
+			if (rn <= r1) {
+				// Linear interpolation
+				double t = (rn - r0) / (r1 - r0);
+				return cd0 + (cd1 - cd0) * t;
+			}
+			r0 = r1;
+			cd0 = cd1;
+		}
+		// Shouldn't ever get to here
+		return cd0;
+	}
 
 	/** Displacement from target, starting with negative range (m) */
 	public double x;
@@ -70,14 +114,14 @@ public class PowerCell {
 		x += vx * t;
 		y += vy * t;
 		vy -= GRAVITY * t;
-//		calculateDrag(t);
+		calculateDrag(t);
 	}
 
 	/** Calculate the aerodynamic drag */
 	private void calculateDrag(double t) {
 		double v = velocity();
 		double drag = 0.5 * AIR_MASS_DENSITY * v * v * REFERENCE_AREA *
-			DRAG_COEFFICIENT;
+			dragCoefficient(reynoldsNumber());
 		// F = ma, so a = F / m
 		double acc = drag / MASS_KG;
 		double dv = acc * t; // delta velocity magnitude
@@ -86,6 +130,12 @@ public class PowerCell {
 		double ay = vy / v;
 		vx -= ax * dv;
 		vy -= ay * dv;
+	}
+
+	/** Calculate the Reynolds number based on velocity */
+	private double reynoldsNumber() {
+		return velocity() * AIR_MASS_DENSITY * REFERENCE_LENGTH /
+			AIR_VISCOSITY_COEFFICIENT;
 	}
 
 	/** Check collision with targets */
@@ -111,5 +161,12 @@ public class PowerCell {
 	public boolean checkRange() {
 		return Port.OUTER.checkRange(this) ||
 		       Port.INNER.checkRange(this);
+	}
+
+	/** Run this class to test drag coefficient function */
+	static public void main(String[] args) {
+		double rn = Double.parseDouble(args[0]);
+		System.out.println("Reynolds number: " + rn);
+		System.out.println("Drag coefficient: " + dragCoefficient(rn));
 	}
 }
